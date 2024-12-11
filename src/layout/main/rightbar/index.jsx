379c0ee6a86872a9da6724e08d7e9098ad531dd8
Kaan Atakan Yilmaz <yilmazkaanatakan@gmail.com>
2024-12-11 10:26:56 +0300

    Rightbar has been added

--- src/layout/main/rightbar/index.jsx
+++ src/layout/main/rightbar/index.jsx
@@ -1,9 +1,18 @@
-function RightBar(){
+import Search from "./search";
+import Premium from "./premium";
+import Topics from "./topics";
+import Follow from "./follow";
+import Footer from "./Footer";
 
-    return(
-       <aside className="w-[350px] mr-2.5">
-        sag sidebar
-       </aside>
-    )
+function RightBar() {
+  return (
+    <aside className="w-[350px] mr-2.5">
+      <Search />
+      <Premium />
+      <Topics />
+      <Follow />
+      <Footer />
+    </aside>
+  );
 }
 export default RightBar;
