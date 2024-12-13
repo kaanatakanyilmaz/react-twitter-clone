a542adea1a105fd20594cd9aaf881965b62a27fa
Kaan Atakan Yilmaz <yilmazkaanatakan@gmail.com>
2024-12-13 11:01:21 +0300

    mock data has been added

---
+++ src/mock/index.js
@@ -0,0 +1,54 @@
+export const whoFollowUsers = [
+  {
+    id: 3,
+    fullName: "Mümtaz Reis",
+    userName: "@" + "mumtazreiss",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1392849581143216128/E_UmsLy1_400x400.jpg",
+    verified: true,
+  },
+
+  {
+    id: 4,
+    fullName: "Bill Gates",
+    userName: "@" + "billgates",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1674815862879178752/nTGMV1Eo_400x400.jpg",
+  },
+  {
+    id: 5,
+    fullName: "Cristiano Ronaldo",
+    userName: "@" + "cristiano",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1594446880498401282/o4L2z8Ay_400x400.jpg",
+  },
+  {
+    id: 6,
+    fullName: "Elon Musk",
+    userName: "@" + "elonmusk",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg",
+  },
+
+  {
+    id: 7,
+    fullName: "Taylor Swift",
+    userName: "@" + "taylorswift13",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1766836345450672128/HpuBz514_400x400.jpg",
+  },
+  {
+    id: 8,
+    fullName: "Jeff Bezos",
+    userName: "@" + "jeffbezos",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1591558315254890500/ETIHb4Nl_400x400.jpg",
+  },
+  {
+    id: 9,
+    fullName: "Selena Gomez",
+    userName: "@" + "selenagomez",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1758176482923544576/yqC9eTZh_400x400.jpg",
+  },
+];
