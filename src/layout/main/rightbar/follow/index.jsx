2b145097b14dfd8ced8e04ef5bfc2c39926dc554
Kaan Atakan Yilmaz <yilmazkaanatakan@gmail.com>
2024-12-13 10:57:40 +0300

    Follow component has been updated

--- src/layout/main/rightbar/follow/index.jsx
+++ src/layout/main/rightbar/follow/index.jsx
@@ -1,51 +1,26 @@
-import Button from "../../../../components/button";
-import { useAccounts, useAccount } from "../../../../store/auth/hooks";
+import { Link } from "react-router-dom";
+import { whoFollowUsers } from "../../../../mock";
+import UserCard from "../../../../components/user-card";
 
 function Follow() {
-  const follows = useAccounts();
-  const currentAccount = useAccount();
-
-  console.log("mevcut hesap", currentAccount);
-  console.log("takipçiler", follows);
-
   return (
-    <div className="h-[297px] mb-4 border border-[#2f3336] rounded-2xl">
-      <div className="h-[326px] flex flex-col py-3 px-4">
-        <h5 className="text-[20px] h-[48px] leading-6 font-bold ">
+    <Link to="/" className="">
+      <div className="border border-[#2f3336] rounded-2xl  ">
+        <h5 className="text-[20px] leading-6 font-bold py-3 px-4">
           Kimi takip etmeli
         </h5>
 
-        {follows
-          ?.filter((account) => account.id !== currentAccount.id)
-          .slice(0, 3)
-          .map((account, index) => (
-            <div key={index} className="w-full h-[65px]  ">
-              <div className="outline-none hover:bg-[#eff3f41a] w-full transitions-colors flex">
-                <img
-                  src={account.avatar}
-                  className="w-10 h-10 rounded-full"
-                  alt=""
-                />
+        {whoFollowUsers.slice(0, 3).map((user) => (
+          <UserCard user={user} key={user.id} />
+        ))}
 
-                <div className="mx-3 text-[15px] text-left">
-                  <h6 className="font-bold">{account.userName}</h6>
-                  <div className="text-[#71767b]">{account.fullName}</div>
-                </div>
-
-                <div className="ml-auto my-auto flex justify-center items-center">
-                  <Button size="whiteNormal">Takip et</Button>
-                </div>
-              </div>
-            </div>
-          ))}
-
-        <div className="h-[52px]   text-left">
+        <div className=" text-left py-3 px-4">
           <a href="explore" className="text-[#1d9bf0]">
             Daha fazla göster
           </a>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 export default Follow;
